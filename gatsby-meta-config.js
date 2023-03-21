c075ce1fc39de5cdf069a9768fea473463123fd0
ggam-nyang <v44ads@naver.com>
2023-03-21 17:38:16 +0900

    블로그 시작

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `ggam-nyang.com`,
+  description: `깜냥로그`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://ggam-nyang.github.io`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `ggam-nyang/ggam-nyang.github.io`, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `이재윤`,
     bio: {
-      role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      role: `백엔드 개발자`,
+      description: ['함께 일하고 싶은', '능동적으로 일하는', '이로운 것을 만드는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/ggam-nyang`,
+      linkedIn: `https://www.linkedin.com/in/%EC%9E%AC%EC%9C%A4-%EC%9D%B4-b90b22242/`,
+      email: `v44ads@naver.com`,
     },
   },
 
@@ -43,12 +43,21 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
+        date: '2021.07 ~ 2021.12',
+        activity: 'SW 사관학교 정글 2기 수료',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: 'https://swjungle.net/',
+          github: '',
+          demo: '',
+        },
+      },
+      {
+        date: '2022.02 ~ 2022.08',
+        activity: '백엔드 개발자 근무',
+        links: {
+          post: 'https://togi.co/',
+          github: '',
+          demo: '',
         },
       },
     ],
@@ -72,15 +81,15 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
+        title: '[안드로이드] 하톡시그널',
         description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+          '3:3 미팅 어플',
+        techStack: ['kotlin', 'android'],
+        thumbnailUrl: '',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: '',
+          github: 'https://github.com/on-signal/heartalk-frontend',
+          demo: '',
         },
       },
     ],
